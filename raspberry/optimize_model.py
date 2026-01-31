914576b6785f94259bd36813225a3ae85145cda5
carlomazzanti <carlomazzanti00@gmail.com>
2026-01-31 16:38:54 +0100

    optimization

--- raspberry/optimize_model.py
+++ raspberry/optimize_model.py
@@ -1,16 +1,20 @@
 import tensorflow as tf
 
-# carica il tuo modello Keras
 model = tf.keras.models.load_model("models/csi_hr_best_200.keras")
 
-# crea un convertitore TFLite
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 
-# abilita ottimizzazione (quantizzazione automatica)
-converter.optimizations = [tf.lite.Optimize.DEFAULT]
+# abilita operazioni select TF (necessario per LSTM)
+converter.target_spec.supported_ops = [
+    tf.lite.OpsSet.TFLITE_BUILTINS,    # tutte le ops TFLite standard
+    tf.lite.OpsSet.SELECT_TF_OPS       # ops TF “extra”
+]
 
-# converti in TFLite
+# disabilita lowering delle TensorList ops (richiesto)
+converter._experimental_lower_tensor_list_ops = False
+
 tflite_model = converter.convert()
+
 
 # salva il modello convertito
 with open("models/csi_hr_best_200.tflite", "wb") as f:
